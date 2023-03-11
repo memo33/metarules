a31e548c96b03e81cca7aa8527a9f3720bfd1091
memo <memo33@users.noreply.github.com>
2023-03-11 15:36:40 +0100

    add a RuleGenerator test including new compatibility syntax

---
+++ src/test/scala/meta/ruleGeneratorSpec.scala
@@ -0,0 +1,61 @@
+package metarules.meta
+
+import org.scalatest.{WordSpec, Matchers}
+
+class RuleGeneratorSpec extends WordSpec with Matchers {
+
+  "RuleGenerator" should {
+    "should support meta syntax and RUL2-compatibility syntax" in {
+      import Network._, RotFlip._, Flags._, Implicits._, Group.SymGroup._
+
+      val resolve = Map[Tile, IdTile](
+        (Dirtroad~NS, IdTile(0x57000000, R0F0)),
+        (Dirtroad~EW, IdTile(0x57000000, R1F0)),
+        (Mis~NS,      IdTile(0x57020000, R0F0)),
+        (Mis~EW,      IdTile(0x57020000, R1F0)),
+        (Mis~SN,      IdTile(0x57020000, R2F0)),
+        (Mis~WE,      IdTile(0x57020000, R3F0)),
+        (L1Rhw2~NS,   IdTile(0x57100000, R0F0)),
+        (L1Rhw2~EW,   IdTile(0x57100000, R1F0)))
+
+      val generator = new RuleGenerator {
+        val resolver = resolve
+        def start(): Unit = {
+          Rules += Mis~WE    | (Dirtroad ~> Mis)~WE
+          Rules += L1Rhw2~WE | (Dirtroad ~> L1Rhw2)~WE
+          Rules += IdTile(0xFF000100, R0F0, Cyc1) | (Dirtroad ~> Mis)~WE
+          Rules += (Dirtroad ~> Mis)~WE | IdTile(0xFF000200, R2F0, (Road~EW).symmetries)
+          Rules += L1Rhw2~WE | IdTile(0xFF000300, R0F0, (Road~EW).symmetries) | % | IdTile(0xFF000400, R0F0, (Road~EW).symmetries)
+          Rules += IdTile(0xFF000500, R1F0, (Road~EW).symmetries) | L1Rhw2~WE | IdTile(0xFF000600, R1F0, (Road~EW).symmetries) | %
+          createRules()
+        }
+      }
+
+      generator.start()
+
+      def parseRule(xs: Int*) = xs.grouped(3).map{ case Seq(i,r,f) => IdTile(i, RotFlip(r, f)) }.toSeq match { case Seq(a,b,c,d) => Rule[IdTile](a,b,c,d) }
+
+      generator.queue shouldBe Seq(
+        parseRule(0x57020000,3,0, 0x57000000,1,0, 0x57020000,3,0, 0x57020000,3,0),
+        parseRule(0x57020000,3,0, 0x57000000,3,0, 0x57020000,3,0, 0x57020000,3,0),
+        parseRule(0x57020000,1,0, 0x57000000,3,0, 0x57020000,1,0, 0x57020000,1,0),
+        parseRule(0x57020000,1,0, 0x57000000,1,0, 0x57020000,1,0, 0x57020000,1,0),
+        parseRule(0x57100000,1,0, 0x57000000,1,0, 0x57100000,1,0, 0x57100000,1,0),
+        parseRule(0x57100000,3,0, 0x57000000,3,0, 0x57100000,3,0, 0x57100000,3,0),
+        parseRule(0xFF000100,0,0, 0x57000000,1,0, 0xFF000100,0,0, 0x57020000,3,0),
+        parseRule(0xFF000100,0,0, 0x57000000,3,0, 0xFF000100,0,0, 0x57020000,3,0),
+        parseRule(0xFF000100,0,0, 0x57000000,1,1, 0xFF000100,0,0, 0x57020000,3,1),
+        parseRule(0xFF000100,0,0, 0x57000000,3,1, 0xFF000100,0,0, 0x57020000,3,1),
+        parseRule(0x57000000,1,0, 0xFF000200,2,0, 0x57020000,3,0, 0xFF000200,2,0),
+        parseRule(0x57000000,1,0, 0xFF000200,0,0, 0x57020000,3,0, 0xFF000200,0,0),
+        parseRule(0x57000000,3,0, 0xFF000200,2,0, 0x57020000,3,0, 0xFF000200,2,0),
+        parseRule(0x57000000,3,0, 0xFF000200,0,0, 0x57020000,3,0, 0xFF000200,0,0),
+        parseRule(0x57100000,1,0, 0xFF000300,0,0, 0x57100000,1,0, 0xFF000400,0,0),
+        parseRule(0x57100000,1,0, 0xFF000300,2,0, 0x57100000,1,0, 0xFF000400,2,0),
+        parseRule(0x57100000,3,0, 0xFF000300,0,0, 0x57100000,3,0, 0xFF000400,0,0),
+        parseRule(0x57100000,3,0, 0xFF000300,2,0, 0x57100000,3,0, 0xFF000400,2,0),
+        parseRule(0xFF000500,1,0, 0x57100000,1,0, 0xFF000600,1,0, 0x57100000,1,0),
+        parseRule(0xFF000500,3,0, 0x57100000,3,0, 0xFF000600,3,0, 0x57100000,3,0))
+    }
+  }
+}
